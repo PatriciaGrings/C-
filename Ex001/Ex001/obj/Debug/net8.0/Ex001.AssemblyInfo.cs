a66d26d4f56bccc5407641602c5d128aa07e5476
Patricia Grings <patriciagrings@hotmail.com>
2024-11-12 20:10:32 -0300

    atualizando

--- Ex001/Ex001/obj/Debug/net8.0/Ex001.AssemblyInfo.cs
+++ Ex001/Ex001/obj/Debug/net8.0/Ex001.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Ex003")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Ex001")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffeb0f06607e3db58cbbe80760c5bb80c26f2da1")]
-[assembly: System.Reflection.AssemblyProductAttribute("Ex003")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Ex003")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4842d83c416820fb3da7f346d44569200161da3")]
+[assembly: System.Reflection.AssemblyProductAttribute("Ex001")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Ex001")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
